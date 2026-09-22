95f0bb5e82a9e1c1b046220591f65db8a44bedee
TAMAVERSE <ilhamnp22@gmail.com>
2026-09-22 21:50:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2254,8 +2254,8 @@ let isHost = false;
 window.bikinRoomLatihan = async () => {
     // 🛑 Menggunakan SweetAlert2 Dropdown dengan Optgroup
     const { value: modulId } = await Swal.fire({
-        title: 'Bikin Room Mabar 🎮',
-        text: 'Pilih modul materi yang mau dikerjakan bareng:',
+        title: 'CREATE ROOM',
+        text: 'Pilih modul materi yang ingin dikerjakan bersama:',
         input: 'select',
         inputOptions: {
             'Hukum Dasar (Substansi)': {
@@ -2406,14 +2406,14 @@ window.tampilkanWaitingRoom = function(kode, isHost) {
     document.getElementById('feedbackBox').style.display = 'none';
     
     let btnMulai = isHost ? 
-        `<button onclick="window.mulaiUjianRoom('${kode}')" style="background:var(--success); color:white; padding:15px 30px; border:none; border-radius:8px; font-size:1.2rem; font-weight:bold; cursor:pointer; margin-top:10px; width:100%; box-shadow: 0 4px 10px rgba(0,0,0,0.2);">🚀 MULAI LATIHAN BARENG</button>` : 
+        `<button onclick="window.mulaiUjianRoom('${kode}')" style="background:var(--success); color:white; padding:15px 30px; border:none; border-radius:8px; font-size:1.2rem; font-weight:bold; cursor:pointer; margin-top:10px; width:100%; box-shadow: 0 4px 10px rgba(0,0,0,0.2);">🚀 MULAI</button>` : 
         `<div style="background:#fff3e0; border:1px solid #ffe0b2; padding:15px; border-radius:8px; margin-top:10px; color:#e67e22; font-weight:bold; font-size:1.1rem;"><i class="fas fa-spinner fa-spin"></i> Menunggu Host Memulai Ujian...</div>`;
 
     qText.innerHTML = `
         <div style="text-align:center; padding: 40px; background:white; border-radius:15px; box-shadow:0 10px 30px rgba(0,0,0,0.05); max-width:600px; margin:0 auto; border-top:8px solid var(--primary);">
             <i class="fas fa-users" style="font-size:4rem; color:var(--primary); margin-bottom:15px;"></i>
             <h2 style="color:var(--primary); margin-bottom:5px;">WAITING ROOM</h2>
-            <p style="color:#666; font-size:1rem; margin-bottom:20px;">Berikan kode ini ke teman lu untuk bergabung:</p>
+            <p style="color:#666; font-size:1rem; margin-bottom:20px;">Berikan kode ini ke user lain untuk bergabung:</p>
             
             <div style="background:#f1f8e9; border:2px dashed var(--success); padding:15px; border-radius:10px; font-size:3.5rem; font-weight:900; color:var(--success); letter-spacing:8px; margin-bottom:20px;">
                 ${kode}
@@ -2442,7 +2442,7 @@ window.mulaiUjianRoom = async (kode) => {
     // Pake PROTAMA.confirm biar popup modern dan gak diblokir browser
     const yakin = await PROTAMA.confirm(
         "MULAI LATIHAN?", 
-        "Pastikan semua peserta udah masuk room. Gas mulai sekarang?"
+        "Pastikan semua peserta sudah masuk room"
     );
     
     if (!yakin) return;
